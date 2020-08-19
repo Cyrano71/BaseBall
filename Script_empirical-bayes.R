9697e5ef442374b102e2ac903f51cb4c482ee74c
Cyrano75 <berthierjg@gmail.com>
2020-08-19 23:48:58 +0200

    update model

--- Script_empirical-bayes.R
+++ Script_empirical-bayes.R
@@ -31,10 +31,12 @@ myinits <- list(list(alpha = 100, beta = 275),
 
 parameters <- c("p","alpha","beta")
 
-model.path <- paste0(getwd(),"/models/ModelEmpiricalBayes.bug")
+model.id <- 2
+model.name <- paste0("/models/ModelEmpiricalBayes",model.id,".bug") 
+model.path <- paste0(getwd(), model.name )
 
 samples <- bugs(data,parameters,inits=myinits , model.file = model.path, 
- n.chains=2,n.iter= 8000, n.burnin=500, n.thin=20, DIC=T, 
+ n.chains=2,n.iter= 5000, n.burnin=500, n.thin=20, DIC=T, 
 bugs.directory=bugsdir, codaPkg=F, debug=T)
 
 alpha0 <- mean(samples$sims.list$alpha)
@@ -50,4 +52,12 @@ career_filtered %>%
 
 career_eb <- career %>%
   mutate(eb_estimate = (H + alpha0) / (AB + alpha0 + beta0))
+
+if(model.id == 2){
+  pAaron <-   samples$sims.list$p[,which(career_filtered$name == "Hank Aaron")]
+  hist(pAaron)
+  PEP_Aaron <- sum(pAaron < 0.3) / length(pAaron)
+}
+
+
 
